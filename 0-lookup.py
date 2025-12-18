d8bbb9fa72d927048470fd62a05b29429cf6ca84
root <root@ip-10-42-146-136.ec2.internal>
2025-12-18 06:50:02 +0000

    Delet

--- 0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines a function that returns the available attributes and methods of an
-object.
-"""
-
-
-def lookup(obj):
-    """Returns a list containing the attributes and methods of an object."""
-    return dir(obj)
