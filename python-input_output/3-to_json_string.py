c159c8146ede39ae9a77d1ff0d65bb237b7735a8
root <root@ip-10-42-156-204.ec2.internal>
2025-12-26 13:18:32 +0000

    add new task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for JSON serialization."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a Python object."""
+    return json.dumps(my_obj)
