7fd089cd9189670d65265205aca646bb92452327
Fahd36631 <Fahd36631@gmail.com>
2026-01-30 23:20:42 +0300

    fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
-
 function factorial (n) {
-    if (n < 0) {
-      return 1;
-    }
-    if (n === 0) {
+    if (isNaN(n) || n <= 0) {
       return 1;
     }
     return n * factorial(n - 1);
   }
   
-  const arg = Number(process.argv[2]);
-  
-  console.log(factorial(arg));
-  
+  console.log(factorial(Number(process.argv[2])));
